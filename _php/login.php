f456f691e844c96286787d5aaa0e4e019f7a93a0
Leonardo da Silva bravo <leonardosbravo@gmail.com>
2024-04-24 16:09:12 -0300

    estilização padrão aplicada

--- _php/login.php
+++ _php/login.php
@@ -9,14 +9,16 @@
 </head>
 <body>
     <div id="interface">
-        <header id cabecalho>
+        <header id = "cabecalho">
+            <div id="logotipo"><img src="../_imagens/logotipo-doliveira (3).png" alt="Logo Imobiliaria D'Oliveira" id="logo"></div>
             <h1>D' Oliveira Imóveis</h1>
-            <img src="#" alt="Logo Imobiliaria D'Oliveira">
                 <nav id="menu">
                     <h1>Menu</h1>
                     <ul>
                         <li><a href="../_html/index.html">Home</a></li>
-                        <li><a href="../_html/search.html">Buscar Casas</a></li>
+                        <li><a href="../_html/contact.html">Contato</a></li>
+                        <li><a href="../_html/search.html">Buscar Imóveis</a></li>
+                        <li><a href="../_html/about_us.html">Quem Somos</a></li>
                     </ul>
                 </nav>
         </header>
@@ -24,10 +26,22 @@
         <section>
 
         </section>
-
         <footer>
-
-        </footer>
-    </div>
+        <div id="social-links">
+            <a href="https://github.com" target="_blank">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+            <a href="https://instagram.com" target="_blank">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+            <a href="https://youtube.com" target="_blank">
+              <ion-icon name="logo-youtube"></ion-icon>
+            </a>
+        </div>
+    
+        <p>Feito com ♥ pelo <a href="https://github.com/LeonardodaSilvaBravo" target="_blank">Leonardo</a></p>
+          <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+          <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </footer>
 </body>
 </html>
