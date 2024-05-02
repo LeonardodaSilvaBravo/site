7189eeaec33f2787bdd569f54f45e33aabbed4ca
Leonardo da Silva bravo <leonardosbravo@gmail.com>
2024-05-02 11:32:07 -0300

    integração entre login eas senhas criptagrafadas funcionam, falta o cadastro não permitir emails iguais

--- _php/login.php
+++ _php/login.php
@@ -12,14 +12,16 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
         $email = $mysqli->real_escape_string($_POST['email']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
 
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email'";
         $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+        $usuario = $sql_query->fetch_assoc();
 
+        if(password_verify($senha,$usuario['senha']) == true){
         $quantidade = $sql_query->num_rows;
 
         if($quantidade == 1) {
             
-            $usuario = $sql_query->fetch_assoc();
+            
 
             if(!isset($_SESSION)) {
                 session_start();
@@ -35,7 +37,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
         }
 
     }
-
+}
 }
 ?>
 
