a5f0d4f16ee80f633411c9385e56b7cea3d66ddd
Leonardo da Silva bravo <leonardosbravo@gmail.com>
2024-07-02 10:58:53 -0300

    começo da página de atualizar informações dos imóveis

---
+++ _php/alteracao_imoveis.php
@@ -0,0 +1,131 @@
+<?php
+include("conexao.php");
+if(isset($_POST['endereco']) || isset($_POST['ref'])){
+    if(strlen($_POST['endereco']) == 0) {
+        echo "Preencha o endereço";
+    } else if(strlen($_POST['ref']) == 0) {
+        echo "Preencha a referência";
+    } else {
+        $endereco = $mysqli->real_escape_string($_POST['endereco']);
+        $refAtual = $mysqli->real_escape_string($_POST['ref_atual']);
+        $refNova = $mysqli->real_escape_string($_POST['ref_nova']);
+        $areaTer = $mysqli->real_escape_string($_POST['areaTer']);
+        $areaCon = $mysqli->real_escape_string($_POST['AC']);
+        $quartos = $mysqli->real_escape_string($_POST['quartos']);
+        $suites = $mysqli->real_escape_string($_POST['suites']);
+        $banheiros = $mysqli->real_escape_string($_POST['banheiros']);
+        $bairo = $mysqli->real_escape_string($_POST['bairo']);
+        $preco = $mysqli->real_escape_string($_POST['preco']);
+        $oportunidade = $mysqli->real_escape_string($_POST['oportunidade']);
+        $piscina = $mysqli->real_escape_string($_POST['piscina']);
+        $venda_ou_loc = $mysqli->real_escape_string($_POST['venda_ou_loc']);
+        $foto1 = $_FILES['foto1'];
+        $foto2 = $_FILES['foto2'];
+        $foto3 = $_FILES['foto3'];
+        $foto4 = $_FILES['foto4'];
+        $foto5 = $_FILES['foto5'];
+        $foto6 = $_FILES['foto6'];
+        $foto7 = $_FILES['foto7'];
+        $foto8 = $_FILES['foto8'];
+        $foto9 = $_FILES['foto9'];
+        $foto10 = $_FILES['foto10'];
+
+        $pasta = "_imoveis/";
+
+        $foto1Nome = $foto1['name'];
+        $novonome1 = uniqid();
+
+        $extensao1 = strtolower(PATHINFO($foto1Nome, PATHINFO_EXTENSION));
+        $path1 = $pasta . $novonome1 . "." . $extensao1;
+
+        $enviar1 = move_uploaded_file($foto1['tmp_name'], $path1);
+
+        $foto2Nome = $foto2['name'];
+        $novonome2 = uniqid();
+
+        $extensao2 = strtolower(PATHINFO($foto2Nome, PATHINFO_EXTENSION));
+        $path2 = $pasta . $novonome2 . "." . $extensao2;
+
+        $enviar2 = move_uploaded_file($foto2['tmp_name'], $path2);
+
+        $foto3Nome = $foto3['name'];
+        $novonome3 = uniqid();
+
+        $extensao3 = strtolower(PATHINFO($foto3Nome, PATHINFO_EXTENSION));
+        $path3 = $pasta . $novonome3 . "." . $extensao3;
+
+        $enviar3 = move_uploaded_file($foto3['tmp_name'], $path3);
+
+        $foto4Nome = $foto4['name'];
+        $novonome4 = uniqid();
+
+        $extensao4 = strtolower(PATHINFO($foto4Nome, PATHINFO_EXTENSION));
+        $path4 = $pasta . $novonome4 . "." . $extensao4;
+
+        $enviar4 = move_uploaded_file($foto4['tmp_name'], $path4);
+
+        $foto5Nome = $foto5['name'];
+        $novonome5 = uniqid();
+
+        $extensao5 = strtolower(PATHINFO($foto5Nome, PATHINFO_EXTENSION));
+        $path5 = $pasta . $novonome5 . "." . $extensao5;
+
+        $enviar5 = move_uploaded_file($foto5['tmp_name'], $path5);
+
+        $foto6Nome = $foto6['name'];
+        $novonome6 = uniqid();
+
+        $extensao6 = strtolower(PATHINFO($foto6Nome, PATHINFO_EXTENSION));
+        $path6 = $pasta . $novonome6 . "." . $extensao6;
+
+        $enviar6 = move_uploaded_file($foto6['tmp_name'], $path6);
+
+        $foto7Nome = $foto7['name'];
+        $novonome7 = uniqid();
+
+        $extensao7 = strtolower(PATHINFO($foto7Nome, PATHINFO_EXTENSION));
+        $path7 = $pasta . $novonome7 . "." . $extensao7;
+
+        $enviar7 = move_uploaded_file($foto7['tmp_name'], $path7);
+
+        $foto8Nome = $foto8['name'];
+        $novonome8 = uniqid();
+
+        $extensao8 = strtolower(PATHINFO($foto8Nome, PATHINFO_EXTENSION));
+        $path8 = $pasta . $novonome8 . "." . $extensao8;
+
+        $enviar8 = move_uploaded_file($foto8['tmp_name'], $path8);
+
+        $foto9Nome = $foto9['name'];
+        $novonome9 = uniqid();
+
+        $extensao9 = strtolower(PATHINFO($foto9Nome, PATHINFO_EXTENSION));
+        $path9 = $pasta . $novonome9 . "." . $extensao9;
+
+        $enviar9 = move_uploaded_file($foto9['tmp_name'], $path9);
+
+        $foto10Nome = $foto10['name'];
+        $novonome10 = uniqid();
+
+        $extensao10 = strtolower(PATHINFO($foto10Nome, PATHINFO_EXTENSION));
+        $path10 = $pasta . $novonome10 . "." . $extensao10;
+
+        $enviar10 = move_uploaded_file($foto10['tmp_name'], $path10);
+
+        $sql_code = "SELECT * FROM imoveis WHERE endereco = '$endereco'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+        $usuario = $sql_query->fetch_assoc();
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 0) {
+
+        $mysqli-> query("");
+        echo("Cadastro efetuado com sucesso");
+        header("Location: painel_admin.php");
+    } else {
+        echo "Falha ao cadastrar! Endereço já cadastrado";
+    }
+}
+}
+?>
---
+++ _php/alterar_imoveis.php
@@ -0,0 +1,160 @@
+<?php
+    include('protect.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../_css/style.css"/>
+    <link rel="shortcut icon" href="../_imagens/icone.png" type="image/x-icon">
+    <script src="../_javascript/functions.js"></script>
+    <title>D'Oliveira Imóveis</title>
+</head>
+<body>
+    <div id="interface">
+        <header id = "cabecalho">
+            <div id="logotipo"><img src="../_imagens/logotipo-doliveira (3).png" alt="Logo Imobiliaria D'Oliveira" id="logo"></div>
+            <br>
+                <nav id="menu">
+                    <h1>Menu</h1>
+                    <ul>
+                        <li><a href="../index.html">Home</a></li>
+                        <li><a href="painel_admin.php">Voltar ao Painel</a></li>
+                    </ul>
+                </nav>
+        </header>
+
+        <main>
+            <form enctype="multipart/form-data" action="alteracao_imoveis.php" method="POST">
+                <p>
+                    <label>Referência Atual</label>
+                    <input type="text" name="ref_atual">
+                </p>
+                <p>
+                    <label>Endereço Novo</label>
+                    <input type="text" name="endereco">
+                </p>
+                <p>
+                    <label>Referência Nova</label>
+                    <input type="text" name="ref_nova">
+                </p>
+                <p>
+                    <label>Área do Terreno Novo</label>
+                    <input type="text" name="areaTer">
+                </p>
+                <p>
+                    <label>Área Construída Novo</label>
+                    <input type="text" name="AC">
+                </p>
+                <p>
+                    <label>Número de Quartos Novo</label>
+                    <input type="text" name="quartos">
+                </p>
+                <p>
+                    <label>Número de Suítes Novo</label>
+                    <input type="text" name="suites">
+                </p>
+                <p>
+                    <label>Número de Banheiros Novo</label>
+                    <input type="text" name="banheiros">
+                </p>
+                <p>
+                    <label>Bairro Novo</label>
+                    <input type="text" name="bairo">
+                </p>
+                <p>
+                    <label>Preço Novo</label>
+                    <input type="text" name="preco">
+                </p>
+                <p>
+                    <label>Oportunidade Nova</label>
+                    <input type="text" name="oportunidade">
+                </p>
+                <p>
+                    <label>Venda ou Locação Novo</label> <br>
+                    <input type="radio" name="venda_ou_loc" value="venda" id="venda">Venda <br>
+                    <input type="radio" name="venda_ou_loc" value="locacao" id="venda">Locação <br>
+                </p>
+                <p>
+                    <label>Tem Piscina Agora</label> <br>
+                    <input type="radio" name="piscina" value="sim" id="sim">Sim <br>
+                    <input type="radio" name="piscina" value="nao" id="nao">Não <br>
+                </p>
+                <p>
+                    <label>Foto 1</label>
+                    <input type="file" name="foto1">
+                </p>
+                
+                <p>
+                    <label>Foto 2</label>
+                    <input type="file" name="foto2">
+                </p>
+                <p>
+                    <label>Foto 3</label>
+                    <input type="file" name="foto3">
+                </p>
+                <p>
+                    <label>Foto 4</label>
+                    <input type="file" name="foto4">
+                </p>
+                <p>
+                    <label>Foto 5</label>
+                    <input type="file" name="foto5">
+                </p>
+                <p>
+                    <label>Foto 6</label>
+                    <input type="file" name="foto6">
+                </p>
+                <p>
+                    <label>Foto 7</label>
+                    <input type="file" name="foto7">
+                </p>
+                <p>
+                    <label>Foto 8</label>
+                    <input type="file" name="foto8">
+                </p>
+                <p>
+                    <label>Foto 9</label>
+                    <input type="file" name="foto9">
+                </p>
+                <p>
+                    <label>Foto 10</label>
+                    <input type="file" name="foto10">
+                </p>
+                <p>
+                    <button type="submit">Cadastrar</button>
+                </p>
+            </form>
+        </main>
+
+        <footer>
+
+            <div id="social-links">
+            <a href="https://www.facebook.com/doliveiraimoveisperuibe/" target="_blank">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+            <a href="https://www.instagram.com/doliveiraimoveis/" target="_blank">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+            <a href="https://www.youtube.com/@DOliveiraImoveisPeruibe" target="_blank">
+              <ion-icon name="logo-youtube"></ion-icon>
+            </a>
+            <a href="https://www.tiktok.com/@doliveiraimoveis" target="_blank">
+              <ion-icon name="logo-tiktok"></ion-icon>
+            </a>
+
+        </div>
+
+        <p>Feito com ♥ pelo <a href="https://github.com/LeonardodaSilvaBravo" target="_blank">Leonardo</a></p>
+    </footer>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+</body>
+</html>
