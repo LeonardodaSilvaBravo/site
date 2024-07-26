cf00c7939706042c69e074b13db4fe57fe31c3b1
Leonardo da Silva bravo <leonardosbravo@gmail.com>
2024-07-26 15:18:45 -0300

    correções no style

--- _php/buscar.php
+++ _php/buscar.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="../_css/style.css"/>
     <link rel="shortcut icon" href="../_imagens/icone.png" type="image/x-icon">
     <script src="../_javascript/functions.js"></script>
@@ -67,3 +66,30 @@
                 </p>
             </form>
         </main>
+
+        <footer>
+
+            <div id="social-links">
+                <a href="https://www.facebook.com/doliveiraimoveisperuibe/" target="_blank">
+                <ion-icon name="logo-facebook"></ion-icon>
+                </a>
+                <a href="https://www.instagram.com/doliveiraimoveis/" target="_blank">
+                <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a href="https://www.youtube.com/@DOliveiraImoveisPeruibe" target="_blank">
+                <ion-icon name="logo-youtube"></ion-icon>
+                </a>
+                <a href="https://www.tiktok.com/@doliveiraimoveis" target="_blank">
+                <ion-icon name="logo-tiktok"></ion-icon>
+                </a>
+
+            </div>
+
+            <p>Made by <a href="https://github.com/LeonardodaSilvaBravo" target="_blank" style="text-decoration: none; color: black;">Leonardo</a></p>
+        </footer>
+
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
